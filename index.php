9a1dfc44c8e2f95ae84c10d836d26057e3494e93
Tissot David <david.tissot@ac-grenoble.fr>
2016-10-12 17:22:17 +0200

    Création du projet dans NetBeans. Ajout de la page index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+
+        <ul id="menu_horizontal">
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="club.php?action=clubs">Gestion des clubs</a></li>
+            <li><a href="competition.php?action=competitions">Gestion des compétitions</a></li>
+        </ul>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
